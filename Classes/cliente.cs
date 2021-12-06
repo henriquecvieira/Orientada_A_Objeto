fba22e986094c2ccb191a299c962357d27c9f4d3
Henrique Vieira <87788394+henriquecvieira@users.noreply.github.com>
2021-12-06 12:26:25 -0300

    Add files via upload

---
+++ Classes/cliente.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Cliente
+    {
+        public static string Teste;
+
+        public string Nome;
+        public string Telefone;
+        public string CPF;
+
+        public void Gravar()
+        {
+            //implementar
+        }
+
+        private static string caminhoBaseClientes()
+        {
+            return ConfigurationManager.AppSettings["BaseDeClientes"];
+        }
+
+        public static List<Cliente> LerClientes()
+        {
+            var clientes = new List<Cliente>();
+            return clientes;
+            if (File.Exists(caminhoBaseClientes()))
+            {
+                using (StreamReader arquivo = File.OpenText(caminhoBaseClientes()))
+                {
+                    string linha;
+                    int i = 0;
+                    while ((linha = arquivo.ReadLine()) != null)
+                    {
+                        if (i == 0) continue;
+                        i++;
+                        var clienteArquivo = linha.Split(';');
+                        var cliente = new Cliente();
+                        cliente.Nome = clienteArquivo[0];
+                        cliente.Telefone = clienteArquivo[1];
+                        cliente.CPF = clienteArquivo[2];
+
+                        clientes.Add(cliente);
+                    }
+
+                    Console.WriteLine("===================");
+                }
+
+            }
+            
+            return clientes;
+        }
+        
+    }
+
+}
