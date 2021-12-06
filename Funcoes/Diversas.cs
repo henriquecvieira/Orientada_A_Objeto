215ba1c537cc408ecaa95ca0615233050a3c9458
Henrique Vieira <87788394+henriquecvieira@users.noreply.github.com>
2021-12-06 12:26:42 -0300

    Add files via upload

---
+++ Funcoes/Diversas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcoes
+{
+    class Diversas
+    {
+        public int CalcularSoma2()
+        {
+            int a = 1;
+            int b = 2;
+            int c = a + b;
+            return c;
+        }
+    }
+}
---
+++ Funcoes/Tabuada.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcoes
+{
+    class Tabuada
+    {
+        public static void Calcular(int numero)
+        {
+            Console.WriteLine("===================");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(numero + " x " + i + " = " + (numero * i));
+            }
+            Console.WriteLine("===================");
+        }
+    }
+}
